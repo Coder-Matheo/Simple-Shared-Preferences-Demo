b964cb82f331c55ce10f0fa2a251b87f64d2cf0f
Coder-Matheo <ali.dinarvand1370@gmail.com>
2021-10-10 23:08:02 +0200

    Simple Shared Preferences Demo

--- app/src/main/java/rob/myappcompany/sharedpreferencesdemo/MainActivity.java
+++ app/src/main/java/rob/myappcompany/sharedpreferencesdemo/MainActivity.java
@@ -4,16 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    EditText editText;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,29 +32,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.nameInput);
+        button = findViewById(R.id.buttonPress);
+
 
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor editor = preferences.edit();
-        for (int i = 0; i < 5; i++){
-            editor.putString("Name"+i, "Hamnet"+i);
-        }
+
+        Set<String> getName = new HashSet<>();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getName.add(editText.getText().toString());
+                Snackbar.make(view, "Created", BaseTransientBottomBar.LENGTH_LONG ).show();
+            }
+        });
+
+        getName.add("Ali");
+        getName.add("Matheo");
+        getName.add("Shahram");
+        editor.putStringSet("Name", getName);
+
 
 
         editor.apply();
 
 
         SharedPreferences getPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String name = "";
-        List<String> getName = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            name = getPreferences.getString("Name", "");
-            getName.add(name);
-        }
-        Log.i("Ta", getName.get(0));
-        if (!name.equalsIgnoreCase("")){
 
-            Log.i("Tag", name);
-            textView.setText(name);
+        Set<String> getName1 = new HashSet<>();
+        Set<String> name = getPreferences.getStringSet("Name", getName1);
+
+
+        if (!name.isEmpty()){
+
+
+            textView.setText( name.toString());
 
         }
     }
